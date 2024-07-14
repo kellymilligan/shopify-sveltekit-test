690a2dc2802837bc9e2fd5aadf51f58cc6b71640
Kelly Milligan <kelly@milligan.net.nz>
2024-07-14 22:52:53 +1200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let data;
+	console.log(data.products);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each data.products as product}
+	<p>{product.title}</p>
+{/each}
